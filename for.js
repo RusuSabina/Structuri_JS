aae11ca9ab8473462766a8c97b2f5784c13a4eb8
Sabina Rusu <rususabina0@gmail.com>
2023-01-14 13:25:16 +0200

    added for.js

---
+++ for.js
@@ -0,0 +1,13 @@
+//Folosind instructiunea for, sa se afiseze in consola primele 10 numere naturale, in ordine descrescatoare.
+
+
+console.log("Afisarea in ordine descrescatoare a primelor 10 numere naturale:")
+
+function dec(){
+for (let i=10;i>=1; i--){
+ 
+ console.log( i)
+}
+ 
+}
+dec()
